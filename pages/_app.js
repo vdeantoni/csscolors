aefc167253972bec377f226ad659deadf1deb541
Vinicius De Antoni <vdeantoni@gmail.com>
2022-08-22 10:21:38 -0700

    Adjusted viewport metatag

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>CSS Colors</title>
         <meta name="description" content="Simple tool to browse CSS named colors" />
-        <meta
-          name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
